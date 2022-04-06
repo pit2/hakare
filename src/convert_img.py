781cce410357f5d5180c2173273199f127ce2de7
Martin Boehm <martinboehm@mailbox.org>
2022-04-06 23:54:34 +0200

    feat: Add function to convert imgs to hdf5 file
    Images are now stored as hdf5 file. Also adds a test function to
    show image read from hdf5 file.

--- src/convert_img.py
+++ src/convert_img.py
@@ -4,6 +4,7 @@ from torchvision.utils import save_image
 import os.path
 import csv
 import numpy as np
+import h5py
 
 
 def img_test():
@@ -25,20 +26,20 @@ def img_test():
 
 
 def convert_img_to_array(directory, img_name, labels=[]):
-    """Convert image to flattened nparray with label attached at last position.
+    """Convert image to flattened nparray.
 
-    Each row contains width*height columns for the actual image data, and one column for the label.
+    Each row contains width*height columns for the actual image data.
     This method assumes the label
     is the sole content of a file .char.text in the same directory as the image.
 
     Parameters:
         directory (string): Path to the directory where the image is located.
         img_name (string): The file name of the image, including extension.
-        path_to_csv (string): The path to the csv file to which the line is appended.
         labels (list of strings): Duplicate-free list of strings of previously read labels.
 
     Returns:
         nparray - image converted to 1D array plus numeric label at last position.
+        ind (int) - index of label
         labels parameter (list of strings) augmented by the newly-added label.
     """
 
@@ -51,9 +52,8 @@ def convert_img_to_array(directory, img_name, labels=[]):
         ind = len(labels) - 1
     else:
         ind = labels.index(label)
-    row = np.append(row, ind)
 
-    return row, labels
+    return row, ind, labels
 
 
 def convert_to_csv(directory, path_to_csv, path_to_labels_list, labels=[], limit=0):
@@ -77,19 +77,75 @@ def convert_to_csv(directory, path_to_csv, path_to_labels_list, labels=[], limit
         count = 0
         for root, _, files in os.walk(directory):
             for file in [f for f in files if not f[0] == "."]:
-                row, labels = convert_img_to_array(root, file, labels)
+                row, ind, labels = convert_img_to_array(root, file, labels)
+                row = np.append(row, ind)
                 writer = csv.writer(csv_file)
                 writer.writerow(row)
                 count += 1
                 if count > limit:
                     break
 
-    with open(path_to_labels_list, "w") as file:
+    write_labels(path_to_labels_list, labels)
+
+
+def write_labels(path, labels):
+    with open(path, "w") as file:
         writer = csv.writer(file)
         for i in range(len(labels)):
             writer.writerow([i, labels[i]])
 
 
-convert_to_csv("/Volumes/MACBACKUP/DataSets/images/ETL9G",
-               "/Volumes/MACBACKUP/DataSets/ETL-9-examples.csv", "data/ETL9b-labels-examples.csv", [], 100)
+def convert_to_hdf5(directory, path_to_hdf5, path_to_labels_list, labels=[], limit=0):
+    """Read directory e.g. ETL9 and scan its subdirectories for images. The images and
+    corresponding labels are then saved to a hdf5 file.
 
+    Each subdirectory must contain a collection of images and a .char.txt file with the label as
+    its sole content. The method writes the images that have been converted to arrays to the
+    given hdf5 file, with group key 'images', and group "labels" for the labels.
+    It also saves the labels list as csv (first column index, second is label).
+
+    Parameters:
+        directory (string): Root directory, e.g. data/images/ETL9, for the image files.
+        path_to_hdf5 (string): Path including file name of output hdf5 file.
+        path_to_labels_list (string): Path including file name of labels dictionary csv file.
+        labels (list of strings): List of labels, default empty, but could be converted contents
+            of labels csv dictionary as well.
+        limit (int): number of images to be crawled 0 to crawl all images
+            in the directory.
+    """
+    count = 0
+    img_rows = []
+    labels_column = []
+    for root, _, files in os.walk(directory):
+        for file in [f for f in files if not f[0] == "."]:
+            row, ind, labels = convert_img_to_array(root, file, labels)
+            img_rows.append(row)
+            labels_column.append(ind)
+            count += 1
+            if count > limit:
+                break
+
+    with h5py.File(path_to_hdf5, "a") as hdf5:
+        hdf5.create_dataset("images", data=np.array(img_rows))
+        hdf5.create_dataset("labels", data=np.array(labels_column))
+
+    write_labels(path_to_labels_list, labels)
+
+
+def read_hdf5(filename):
+    """test function to read and show image loaded from hdf5 file."""
+    with h5py.File(filename, "r") as f:
+        key = list(f.keys())[0]
+        data = np.array(list(f[key]))
+        img = data[0].reshape(127, 128)
+        Image.fromarray(img).show()
+
+
+read_hdf5("/Volumes/MACBACKUP/DataSets/ETL-9-examples.hdf5")
+
+#  convert_to_hdf5("/Volumes/MACBACKUP/DataSets/images/ETL9G",
+#                "/Volumes/MACBACKUP/DataSets/ETL-9-examples.hdf5",
+#                "data/ETL9b-labels-hdf5-examples.csv", [], 100)
+
+# convert_to_csv("/Volumes/MACBACKUP/DataSets/images/ETL9G",
+#       "/Volumes/MACBACKUP/DataSets/ETL-9-examples.csv", "data/ETL9b-labels-examples.csv", [], 100)
