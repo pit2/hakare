d8a0f8ab253a22d2e7bc66fa251616a11bf49dca
Martin Boehm <martinboehm@mailbox.org>
2022-04-06 14:09:13 +0200

    Add convert_img_to_csv in convert_img.py

--- src/convert_img.py
+++ src/convert_img.py
@@ -1,17 +1,45 @@
 from PIL import Image
 import torchvision.transforms as transforms
 from torchvision.utils import save_image
+import os.path
+import csv
+import numpy as np
 
-image = Image.open("data/images/ETL1/0x003d/059245.png")
 
-image.show()
+def img_test():
+    """Opens an image, transforms it into tensor, and saves the tensor back as image file."""
 
-transform = transforms.Compose([transforms.PILToTensor()])
+    image = Image.open("data/images/ETL1/0x003d/059245.png")
 
-img_tensor = transform(image)
+    image.show()
 
-img_tensor = img_tensor / 255
+    transform = transforms.Compose([transforms.PILToTensor()])
 
-print(img_tensor)
+    img_tensor = transform(image)
 
-save_image(img_tensor, "data/tmp/back.png")
+    img_tensor = img_tensor / 255
+
+    print(img_tensor)
+
+    save_image(img_tensor, "data/tmp/back.png")
+
+
+def convert_img_to_csv(directory, img_name, path_to_csv, label_list=[]):
+    """Convert image to flattened nparray and write it in a row of csv file.
+
+    Each row contains width*height columns for the actual image data, and one column for the label
+    """
+    path = os.path.join(directory, img_name)
+    image = Image.open(path)
+    row = np.array(image).flatten()
+    with open(os.path.join(directory, ".char.txt"), "r") as txt_file:
+        label = txt_file.read()
+    label_list.append(label)
+    row = np.append(row, len(label_list) - 1)
+    file = open(path_to_csv, "a")
+    writer = csv.writer(file)
+    writer.writerow(row)
+    file.close()
+
+
+convert_img_to_csv("data/images/ETL1/0x003d", "059245.png", "data/test.csv")
