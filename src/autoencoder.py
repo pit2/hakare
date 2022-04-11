cb38e4ede606da9479f8fa49bcc523c68a23be7c
Martin Boehm <martinboehm@mailbox.org>
2022-04-11 15:47:43 +0200

    Refactor code and add helper functions for notebook
    Implements various changes/additions to autoencoder.py to make training, validation, testing and visualization easier.

--- src/autoencoder.py
+++ src/autoencoder.py
@@ -1,12 +1,16 @@
 import torch
-from torchsummary import summary
 import data
+import numpy as np
+from os import path
+from PIL import Image
+import torchvision.transforms as transforms
+from torchvision.utils import save_image
 
 DEVICE = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
 
 class AutoEncoder(torch.nn.Module):
-    def __init__(self):
+    def __init__(self, decode=True):
         """ torch.nn.ConvTranspose2d(
                 128, 64, kernel_size=3, stride=2, output_padding=1),
             torch.nn.ReLU(),
@@ -22,6 +26,7 @@ class AutoEncoder(torch.nn.Module):
         """
         super().__init__()
 
+        self.decode = decode
         self.encoderCNN = torch.nn.Sequential(
             torch.nn.Conv2d(1, 32, kernel_size=3, stride=2, padding=1),
             torch.nn.ReLU(),
@@ -38,7 +43,7 @@ class AutoEncoder(torch.nn.Module):
         )
 
         self.encoderFC = torch.nn.Sequential(
-            torch.nn.Linear(128*2*2, 10),
+            torch.nn.Linear(128*2*2, 25),
             torch.nn.ReLU()
         )
 
@@ -63,7 +68,7 @@ class AutoEncoder(torch.nn.Module):
         )
 
         self.decoderFC = torch.nn.Sequential(
-            torch.nn.Linear(10, 128*2*2),
+            torch.nn.Linear(25, 128*2*2),
             torch.nn.ReLU()
         )
     # with pool padding: 128 * 3 * 3
@@ -74,50 +79,106 @@ class AutoEncoder(torch.nn.Module):
         x = x.view(-1, 128*2*2)
         x = self.encoderFC(x)
         # print(f"Shape after FC encoding: {x.shape}")
-        x = self.decoderFC(x)
-        # print(f"Shape after FC decoding: {x.shape}")
-        x = x.view(-1, 128, 2, 2)
-        # print(f"Shape for decoder CNN: {x.shape}")
-        x = self.decoderCNN(x)
-        # print(f"Shape after decoding: {x.shape}")
+        if self.decode:
+            x = self.decoderFC(x)
+            # print(f"Shape after FC decoding: {x.shape}")
+            x = x.view(-1, 128, 2, 2)
+            # print(f"Shape for decoder CNN: {x.shape}")
+            x = self.decoderCNN(x)
+            # print(f"Shape after decoding: {x.shape}")
         return x
 
 
-def train(X, epochs):
-    model = AutoEncoder().to(DEVICE)
-    summary(model, (1, 128, 128))
-    loss_function = torch.nn.MSELoss()
-    optimizer = torch.optim.Adam(
+def train(model, training_gen, valid_gen, epochs):
+    mse = torch.nn.MSELoss()
+    adam = torch.optim.Adam(
         model.parameters(), lr=1e-1, weight_decay=1e-8)
-    losses = []
+    train_losses = []
+    valid_losses = []
+    min_valid_loss = np.inf
     outputs = []
-    model.train()
-
     for i in range(epochs):
-        print(f"Epoch: {i}")
-        first = True
-        local_loss = []
-        for img, _ in X:
-            if first:
-                print(torch.cuda.memory_allocated()/1024**2)
-            img = img.to(DEVICE)
-            if first:
-                print(torch.cuda.memory_allocated()/1024**2)
-            out = model(img)
-            loss = loss_function(img, out)
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-            local_loss.append(loss.item())
-            if first:
-                outputs.append((img, out))
-            first = False
-            del loss, out, img
-        losses.append(np.mean(local_loss))
-        torch.cuda.empty_cache()
+        model, train_losses, outputs = step(
+            model, training_gen, mse, adam, train_losses, outputs)
+        print(f"Error in epoch {i}: {train_losses[-1]}")
+        valid_losses = eval_model(model, valid_gen, mse, valid_losses)
+        if min_valid_loss > valid_losses[-1]:
+            torch.save(model, path.join("..", "data", "models", "model.pt"))
+    return model, train_losses, valid_losses
 
 
-dataset = data.Characters(data.PATH_TO_DATA_SHORT, 128, 128, 1)
-X_train, _, _ = data.split(dataset)
+def step(model, generator, loss_function, optimizer, losses, outputs):
+    model.train()
+    first = True
+    local_loss = []
+    for imgs, _ in generator:
+        #    print(torch.cuda.memory_allocated()/1024**2)
+        imgs = imgs.to(DEVICE)
 
-train(X_train, 10)
+        decoded = model(imgs)
+        loss = loss_function(imgs, decoded)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        local_loss.append(loss.item())
+        if first:
+            outputs.append((imgs, decoded))
+        first = False
+    err = np.mean(local_loss)
+    losses.append(err)
+    torch.cuda.empty_cache()
+    return model, losses, outputs
+
+
+def eval_model(model, generator, loss_function, losses):
+    model.eval()
+    local_errors = []
+    with torch.no_grad():
+        for imgs, _ in generator:
+            decoded = model(imgs)
+            loss = loss_function(imgs, decoded)
+            local_errors.append(loss.item())
+        losses.append(np.mean(local_errors))
+
+    return losses
+
+
+def reconstruct(img_path, model):
+    img_tensor = _img_to_tensor(img_path)
+
+    model.eval()
+    reconstructed_tensor = model(img_tensor)
+    reconstructed_tensor = reconstructed_tensor.view(1, 128, 128)
+    save_image(img_tensor, path.join("..", "data", "tmp", "back.png"))
+    print(img_tensor - reconstructed_tensor)
+
+    transform = transforms.ToPILImage()
+    reconstructed = transform(reconstructed_tensor)
+    return reconstructed
+
+
+def encode(img_path, model):
+
+    img_tensor = _img_to_tensor(img_path)
+    img_tensor = img_tensor.view(1, 1, 128, 128)
+    model.decode = False
+    model.eval()
+    encoded_tensor = model(img_tensor)
+    return encoded_tensor
+
+
+def _img_to_tensor(img_path):
+    img = Image.open(img_path)
+    img = img.resize((128, 128))
+    transform = transforms.Compose([transforms.PILToTensor()])
+    img_tensor = transform(img)
+    img_tensor = img_tensor / 255
+
+    return img_tensor
+
+
+def execute():
+    dataset = data.Characters(data.PATH_TO_DATA_SHORT, 128, 128, 1)
+    X_train, _, _ = data.split(dataset)
+
+    train(X_train, 10)
