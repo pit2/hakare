82b98bd3b5f25315c84ad80493a0894b6e0d6c47
Martin Boehm <martinboehm@mailbox.org>
2022-04-12 16:09:43 +0200

    feat: Add train/eval functions for CNN model Closes #5

--- src/model.py
+++ src/model.py
@@ -1,17 +1,14 @@
 import torch
 import data
 import numpy as np
-from os import path
-from PIL import Image
-import torchvision.transforms as transforms
-from torchvision.utils import save_image
 from torchsummary import summary
-import matplotlib.pyplot as plt
 
 DEVICE = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
 
 class Recognizer(torch.nn.Module):
+    """CNN to recognize 128x128 greyscale images of handwritten Japanese characters."""
+
     def __init__(self):
         super().__init__()
 
@@ -31,6 +28,7 @@ class Recognizer(torch.nn.Module):
         self.valid_losses = []
         self.test_loss = np.inf
         self.loss_fn = torch.nn.CrossEntropyLoss()
+        self.min_valid_loss = np.inf
 
     def forward(self, x):
         x = self.cnn(x)
@@ -38,36 +36,92 @@ class Recognizer(torch.nn.Module):
         return self.fc(x)
 
 
-def execute():
-    model = Recognizer().to(DEVICE)
-    summary(model, (1, 128, 128))
+def train(model, train_gen, valid_gen, params={"lr": 1e-1, "weight_decay": 1e-8},
+          epochs=10, report=False):
+    """Using adams optimizer, train and validate the model. Returns the model with the smallest
+    loss on the validation set after given number of epochs.
 
-    dataset = data.Characters(data.PATH_TO_DATA_SHORT, 128, 128, 1)
-    train, valid, test = data.split(dataset, batch_size=32)
+    Parameters:
+    model (Recognizer): CNN model to be trained and validated
+    train_gen (DataLoader): training data (Characters class)
+    valid_gen (DataLoader): validation data (Characters class)
+    params (dictionary): Dictionary with keys lr and weight_decay to hold parameters for
+                        adams optimizer
+    epochs (int): number of epochs for training
+    report (Boolean): true to print test/validation error after each epoch
 
+    Returns best Recognizer model (i.e. min validation error)
+    """
+    model.to(DEVICE)
     model.train()
+    best_model = model
+
+    for i in range(epochs):
+        model, train_loss = _step(model, train_gen, params)
+        model.train_losses.append(train_loss)
+
+        valid_loss = evaluate(model, valid_gen)
+
+        if report:
+            print(
+                f"Epoch {i} --- test error: {train_loss} --- validation error: {valid_loss}")
+
+        if model.min_valid_loss > valid_loss:
+            best_model = model
+
+    return best_model
+
+
+def _step(model, train_gen, params):
+    """Execute one training step (one batch) and return updated model and mean loss."""
     optimizer = torch.optim.Adam(
-        model.parameters(), lr=1e-1, weight_decay=1e-8)
-    for i in range(25):
+        model.parameters(), lr=params["lr"], weight_decay=params["weight_decay"])
+    losses = []
+    for img, target in train_gen:
+        img = img.to(DEVICE)
+        target = target.to(DEVICE)
+        predict = model(img)
+        loss = model.loss_fn(predict, target)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        losses.append(loss.item())
 
-        local_loss = []
-        for imgs, targets in train:
-            #    print(torch.cuda.memory_allocated()/1024**2)
-            imgs = imgs.to(DEVICE)
+    return model, np.mean(losses)
 
-            predicts = model(imgs)
-            loss = model.loss_fn(predicts, targets)
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-            local_loss.append(loss.item())
 
-        err = np.mean(local_loss)
-        model.train_losses.append(err)
-        print(f"Error in epoch {i}: {err}")
-        torch.cuda.empty_cache()
+def evaluate(model, test_gen):
+    """Evaluates the model and returns the loss.
 
-    plt.plot(model.train_losses)
+    Parameters:
+    model (Recognizer): Model to be evaluated.
+    test_gen (DataLoader): Test (or validation) data to evaluate the model.
+
+    Returns mean loss on test/validation data.
+    """
+    model.eval()
+    losses = []
+    with torch.no_grad():
+        for img, target in test_gen:
+            img.to(DEVICE)
+            target.to(DEVICE)
+            predict = model(img)
+            loss = model.loss_fn(predict, target)
+            losses.append(loss.item())
+
+    return np.mean(losses)
+
+
+def print_topology(model): summary(model, (1, 128, 128))
+
+
+def execute():
+    dataset = data.Characters(data.PATH_TO_DATA_SHORT, 128, 128, 1)
+    train_data, valid, test = data.split(dataset, batch_size=32)
+
+    model = train(Recognizer(), train_data, valid)
+    model.test_loss = evaluate(model, test)
+    print(model.test_loss)
 
 
 execute()
